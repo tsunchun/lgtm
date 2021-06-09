68c4d1ab84ffcd864b666079f38737f0107c9314
syunshun <60087916+tsunchun@users.noreply.github.com>
2021-06-09 09:53:51 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     packages=find_packages(exclude=('test',)),
     install_requires=[
         'Click~=7.0',
-        'Pillow~=6.2.0',
+        'Pillow~=8.2.0',
         'requests~=2.22.0',
     ],
     entry_points={
